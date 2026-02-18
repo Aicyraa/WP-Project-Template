43db77a609961cf27b7ad994197bc42121f26a2b
Aicyra <182109647+Aicyraa@users.noreply.github.com>
2026-02-18 19:40:21 +0800

    webpack configs (development, production)

---
+++ webpack.prod.js
@@ -0,0 +1,39 @@
+import path from "node:path"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+
+export default {
+   mode: "production",
+   entry: "./src/script.js",
+   output: {   
+      filename: "main.js",
+      path: path.resolve(import.meta.dirname, "dist"),
+      clean: true
+   },
+
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: "./src/index.html",
+         // favicon: "./src/favicon.ico"
+      })
+   ],
+
+   module: {
+      rules: [
+         {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+         },
+
+         {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+         },
+         {
+            test: /\.html$/i,
+            use: ["html-loader"],
+         },
+      ],
+   }
+
+
+}
